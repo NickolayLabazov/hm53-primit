6caf86d49f9e24ac501e8e35ff5cdbd81b631403
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-02-17 17:50:34 +0300

    correction 3

--- src/function.js
+++ src/function.js
@@ -1,4 +1,14 @@
 export default function descript(obj) {
-  const result = `${(obj.type === 'Bowman') && String.fromCodePoint(0x1F620) || (obj.type === 'Swordsman') && String.fromCodePoint(0x1F621) || (obj.type === 'Magician') && String.fromCodePoint(0x1F9D0) || (obj.type === 'Daemon') && String.fromCodePoint(0x1F47F) || (obj.type === 'Undead') && String.fromCodePoint(0x1F480) || (obj.type === 'Zombie') && String.fromCodePoint(0x1F9DF)}${obj.name[0]}(${obj.level}) ${String.fromCodePoint(0x2694)} ${obj.attack} ${String.fromCodePoint(0x1F6E1)} ${obj.defence} ${String.fromCodePoint(0x2764)} ${obj.health}`;
+  const smileBowman = (obj.type === 'Bowman') && String.fromCodePoint(0x1F620);
+  const smileSwordsman = (obj.type === 'Swordsman') && String.fromCodePoint(0x1F621);
+  const smileMagician = (obj.type === 'Magician') && String.fromCodePoint(0x1F9D0);
+  const smileDaemon = (obj.type === 'Daemon') && String.fromCodePoint(0x1F47F);
+  const smileUndead = (obj.type === 'Undead') && String.fromCodePoint(0x1F480);
+  const smileZombie = (obj.type === 'Zombie') && String.fromCodePoint(0x1F9DF);
+  const typeSmile = smileBowman || smileSwordsman || smileMagician || smileDaemon || smileUndead || smileZombie;
+  const smileAtack = String.fromCodePoint(0x2694);
+  const smileDefence = String.fromCodePoint(0x1F6E1);
+  const smileHealth = String.fromCodePoint(0x2764);
+  const result = `${typeSmile}${obj.name[0]}(${obj.level}) ${smileAtack} ${obj.attack} ${smileDefence} ${obj.defence} ${smileHealth} ${obj.health}`;
   return result;
 }
